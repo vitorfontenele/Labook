5bc1a64cbbbc639032848b92505c6947543ba892
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-10 18:45:43 -0300

    Add PostDatabase methods

--- src/database/PostDatabase.ts
+++ src/database/PostDatabase.ts
@@ -1,5 +1,31 @@
-export class PostDatabase {
-    public async getPosts(){
-        
+import { BaseDatabase } from "./BaseDatabase";
+
+export class PostDatabase extends BaseDatabase {
+    public static TABLE_POSTS = "posts";
+
+    public async findPosts(){
+        const result = await BaseDatabase
+            .connection(PostDatabase.TABLE_POSTS);
+        return result;
+    }
+
+    public async createPost(newPostDB){
+        await BaseDatabase
+            .connection(PostDatabase.TABLE_POSTS)
+            .insert(newPostDB);
+    }
+
+    public async updatePostById(updatedPostDB, id : string){
+        await BaseDatabase
+            .connection(PostDatabase.TABLE_POSTS)
+            .update(updatedPostDB)
+            .where({ id })
+    }
+
+    public async deletePostById(id : string){
+        await BaseDatabase
+            .connection(PostDatabase.TABLE_POSTS)
+            .del()
+            .where({ id });
     }
 }
