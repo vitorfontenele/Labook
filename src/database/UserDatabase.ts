e15f4506aca4eb784ff6a6b041c1d22c725678c6
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-10 22:58:41 -0300

    Add methods to UserDatabase

--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -3,10 +3,17 @@ import { BaseDatabase } from "./BaseDatabase";
 export class UserDatabase extends BaseDatabase {
     public static TABLE_USERS = "users";
     
-    public async getUsers(){
+    public async findUsers(){
         const usersDB = await BaseDatabase
             .connection(UserDatabase.TABLE_USERS);
 
         return usersDB;
+    }
+
+    public async findUserById(id : string){
+        const [ result ] = await BaseDatabase
+            .connection(UserDatabase.TABLE_USERS)
+            .where({ id });
+        return result;
     }
 }
