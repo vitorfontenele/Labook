ac1f09bab4b1fea5f8637c4fe1d0d9182855aa6a
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-11 02:01:52 -0300

    Update PostController

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -19,4 +19,27 @@ export class PostController {
             }
         }
     }
+
+    public createPost = async (req: Request, res: Response) => {
+        try {
+            const content = req.body.content;
+
+            const input = {
+                content
+            };
+
+            const postBusiness = new PostBusiness();
+            await postBusiness.createPost(input);
+
+            res.status(201).send("Post criado com sucesso");
+        } catch (error) {
+            console.log(error)
+
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message)
+            } else {
+                res.status(500).send("Erro inesperado")
+            }
+        }
+    }
 }
