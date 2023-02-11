ecf8fc40414804b673c377795077c3b41af5109b
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-10 22:42:16 -0300

    Add router to index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import cors from "cors";
 import express from "express";
+import { postRouter } from "./router/postRouter";
 
 // Configurando a instância do express
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Rotas
+app.use("/posts", postRouter);
 
 // Porta
 app.listen(3003, () => {
