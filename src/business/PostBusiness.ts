9b859baf6a159f26a3ed8753ec2a819d2d931ba6
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-11 01:18:53 -0300

    Update PostBusiness & PostDatabase

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -1,21 +1,34 @@
 import { PostDatabase } from "../database/PostDatabase";
+import { UserDatabase } from "../database/UserDatabase";
 import { Post } from "../models/Post";
-import { PostDB } from "../types";
+import { PostDB, UserDB } from "../types";
 
 export class PostBusiness {
     public async getPosts(){
         const postDatabase = new PostDatabase();
-        const postsDB : PostDB[] = await postDatabase.findPosts();
+        const postsDB = await postDatabase.findPosts();
+
+        const userDatabase = new UserDatabase();
+        const usersDB = await userDatabase.findUsers();
 
         const output = postsDB.map(postDB => new Post (
             postDB.id,
-            postDB.creator_id,
             postDB.content,
             postDB.likes,
             postDB.dislikes,
             postDB.created_at,
-            postDB.updated_at
+            postDB.updated_at,
+            getCreator(postDB.creator_id)
         ));
+
+        function getCreator(userId : string){
+            const user = usersDB.find(userDB => userDB.id === userId) as UserDB;
+            
+            return {
+                id: user.id,
+                name: user.name
+            }
+        }
 
         return output;
     }
