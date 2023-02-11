0342d9f219fcff20272eb0cd33bff9d31e4969f8
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-11 18:26:41 -0300

    Add routes to postRouter

--- src/router/postRouter.ts
+++ src/router/postRouter.ts
@@ -6,3 +6,6 @@ const postController = new PostController();
 export const postRouter = express.Router();
 
 postRouter.get("/", postController.getPosts);
+postRouter.post("/", postController.createPost);
+postRouter.put("/:id", postController.updatePostById);
+postRouter.delete("/:id", postController.deletePostById);
