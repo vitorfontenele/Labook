d2114efe6b7583dcef8826be483dd2d3f129d2c4
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-13 16:23:18 -0300

    Add getUsers to UserBusiness

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,8 +1,20 @@
 import { UserDatabase } from "../database/UserDatabase";
+import { User } from "../models/User";
 
 export class UserBusiness {
     public async getUsers(){
         const userDatabase = new UserDatabase();
         const usersDB = await userDatabase.findUsers();
+
+        const output = usersDB.map(userDB => new User(
+            userDB.id,
+            userDB.name,
+            userDB.email,
+            userDB.password,
+            userDB.role,
+            userDB.created_at
+        ));
+
+        return output;
     }
 }
