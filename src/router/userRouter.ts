38972400d76f9159005ca4f0eb98e679f9b58c68
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-13 16:55:30 -0300

    Add userRouter to index

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { UserController } from "../controller/UserController";
 
 const userController = new UserController();
-const userRouter = express.Router();
+export const userRouter = express.Router();
 
 userRouter.get("/", userController.getUsers);
 userRouter.post("/signup", userController.createUser);
